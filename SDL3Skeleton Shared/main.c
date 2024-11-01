92a1c58d3488469e8d88e1d8212218433d4babe5
Branden Byers <branden@brandenbyers.com>
2024-11-01 13:49:58 -0700

    Clean up

---
+++ SDL3Skeleton Shared/main.c
@@ -0,0 +1,8 @@
+//
+//  main.c
+//  SDL3Skeleton
+//
+//  Created by Branden Byers on 11/1/24.
+//
+
+#include "main.h"
---
+++ SDL3Skeleton Shared/main.h
@@ -0,0 +1,13 @@
+//
+//  main.h
+//  SDL3Skeleton
+//
+//  Created by Branden Byers on 11/1/24.
+//
+
+#ifndef main_h
+#define main_h
+
+#include <stdio.h>
+
+#endif /* main_h */
